5764027d85a364ad78219079c0d6c4b4d02e9018
Ashuriwastav07 <ashusriwastav58@gmail.com>
2024-09-08 14:10:20 +0530

    1st Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <>
             <div className="flex place-items-center gap-10 h-20 bg-green-400 px-2 py-2">
-                <img src="src/components/logo.png" alt="" className='h-14 w-14'/>
+                <img src="\src\components\logo.png" alt="" className='h-14 w-14'/>
                 <text className='text-2xl font-bold'>NGO FINDER</text>
             </div>
             
